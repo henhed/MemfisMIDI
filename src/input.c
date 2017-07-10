1f1ff6c95113591a14174d9d724eaf2b062d68c3
Henrik Hedelund <henke.hedelund@gmail.com>
2017-07-10 23:06:07 +0200

    input

---
+++ src/input.c
@@ -0,0 +1,95 @@
+/* Copyright (C) 2017 Henrik Hedelund.
+
+   This file is part of MemfisMIDI.
+
+   MemfisMIDI is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   MemfisMIDI is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with MemfisMIDI.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include <fcntl.h>
+#include <linux/joystick.h>
+
+#include "input.h"
+
+struct _MMInput
+{
+  int fd;
+};
+
+MMInput *
+mm_input_new ()
+{
+  MMInput *input = calloc (1, sizeof (MMInput));
+  assert (input != NULL);
+  input->fd = -1;
+  return input;
+}
+
+void
+mm_input_free (MMInput *input)
+{
+  if (input != NULL)
+    {
+      mm_input_disconnect (input);
+      free (input);
+    }
+}
+
+bool
+mm_input_connect (MMInput *input, const char *pathname)
+{
+  if (input == NULL || pathname == NULL)
+    return false;
+
+  mm_input_disconnect (input);
+
+  input->fd = open (pathname, O_RDONLY);
+  if (input->fd < 0)
+    return false;
+
+  fcntl (input->fd, F_SETFL, O_NONBLOCK);
+
+  return true;
+}
+
+void
+mm_input_disconnect (MMInput *input)
+{
+  if (input != NULL && input->fd >= 0)
+    {
+      close (input->fd);
+      input->fd = -1;
+    }
+}
+
+int
+mm_input_read (const MMInput *input)
+{
+  struct js_event e;
+
+  if (input == NULL || input->fd < 0)
+    return -1;
+
+  while (read (input->fd, &e, sizeof (e)) > 0)
+    {
+      if (e.type & JS_EVENT_INIT)
+        continue;
+      if ((e.type & JS_EVENT_BUTTON) && e.value == 1)
+        return (int) e.number;
+    }
+
+  return -1;
+}
---
+++ src/input.h
@@ -0,0 +1,40 @@
+/* Copyright (C) 2017 Henrik Hedelund.
+
+   This file is part of MemfisMIDI.
+
+   MemfisMIDI is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   MemfisMIDI is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with MemfisMIDI.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef MM_INPUT_H
+#define MM_INPUT_H 1
+
+#include <stdbool.h>
+
+typedef struct _MMInput MMInput;
+
+enum
+{
+  MM_INPUT_PREV_SEQUENCE = 5,
+  MM_INPUT_NEXT_SEQUENCE = 7,
+  MM_INPUT_PREV_CHORD = 4,
+  MM_INPUT_NEXT_CHORD = 6,
+  MM_INPUT_KILL_ALL = 2
+};
+
+MMInput *mm_input_new ();
+void mm_input_free (MMInput *);
+bool mm_input_connect (MMInput *, const char *);
+void mm_input_disconnect (MMInput *);
+int mm_input_read (const MMInput *);
+
+#endif /* ! MM_INPUT_H */
